73abc017838a6793845b65168e61e6f227664462
j32u4ukh <j32u4ukh@gmail.com>
2024-05-12 23:59:13 +0800

    feat: 根據座標所屬區塊的起始值以及長度，對座標做轉換

--- opengaze/__init__.py
+++ opengaze/__init__.py
@@ -12,8 +12,10 @@ OpenCV 的順序為 (width, height), numpy 呈現的順序為 (height, width, 3)
 img = cv2.imread("./data/miko.png")
 shape = img.shape
 print(f"img.shape: {img.shape}")
-height = int(shape[0]/3)
-width = int(shape[1]/3)
+src_height = shape[0]
+src_width = shape[1]
+height = int(src_height/3)
+width = int(src_width/3)
 
 og = OpenGaze(height=height, width=width, radius=50, distance=1)
 zone_rate = math.sqrt(2)
@@ -30,11 +32,15 @@ print(f"basic_img.shape: {basic_img.shape}")
 
 # dst = og.gaze(0.5, 0.2)
 # print(f"dst.shape: {dst.shape}")
-w_n_zone, w_distance_list, w_cum_distance_list = og.initZone(0.5, 400, 700, zone_rate)
-print(f"w_n_zone: {w_n_zone}\nw_distance_list: {w_distance_list}\nw_cum_distance_list: {w_cum_distance_list}")
-h_n_zone, h_distance_list, h_cum_distance_list = og.initZone(0.5, 200, 300, zone_rate)
-print(f"h_n_zone: {h_n_zone}\nh_distance_list: {h_distance_list}\nh_cum_distance_list: {h_cum_distance_list}")
 
+# boundary_list, distance_list
+w_zone, w_boundary_list, w_distance_list = og.initZone(0.5, width, src_width, zone_rate)
+print(f"w_boundary_list: {w_boundary_list}\nw_distance_list: {w_distance_list}")
+h_zone, h_boundary_list, h_distance_list = og.initZone(0.5, height, src_height, zone_rate)
+print(f"h_boundary_list: {h_boundary_list}\nh_distance_list: {h_distance_list}")
+
+dst_pivot = og.translate(1920, w_zone, w_boundary_list, w_distance_list)
+print(f"dst_pivot: {dst_pivot}")
 # cv2.imwrite("./data/gaze.png", dst)
 # cv2.imshow("PON", dst)
 # cv2.waitKey(0)
--- opengaze/gaze.py
+++ opengaze/gaze.py
@@ -37,17 +37,14 @@ class OpenGaze:
         self.channel = 0
         self.distance = distance
 
-    def initZone(self, pivot: float, dst_length: int, src_length: int, rate: float):
-        def sort_boundry(boundary):
-            return boundary[0]
-        
+    def initZone(self, pivot: float, dst_length: int, src_length: int, rate: float):        
         dst_center = pivot * dst_length
         src_center = pivot * src_length
         boundary_list = [DstSrc(dst_center, src_center)]
         self.initLeftZone(dst_center, src_center, rate, boundary_list)
         self.initRightZone(dst_center, src_center, dst_length, rate, boundary_list)
 
-        boundary_list.sort()      
+        boundary_list.sort()
         n_zone = len(boundary_list)
         distance_list = []
 
@@ -75,7 +72,8 @@ class OpenGaze:
             distance_list[1].src += distance_list[0].src
             distance_list.pop(0)
             boundary_list.pop(0)
-                
+        
+        n_zone = len(boundary_list)
         return n_zone, boundary_list, distance_list
     
     def initLeftZone(self, dst_boundary: float, src_boundary: float, rate: float, boundary_list: List[float]):
@@ -100,29 +98,30 @@ class OpenGaze:
             # else:
             #     boundary_list.append(length - 1)
     
-    def getZone(self, distance: float, is_width: bool) -> int:
-        is_neg = distance < 0
-        distance = math.fabs(distance)
-        if is_width:
-            for zone in range(self.w_zone_number):
-                if distance <= self.w_cum_distance_list[zone]:
-                    return zone
-            return self.w_zone_number - 1
-        else:
-            for zone in range(self.h_zone_number):
-                if distance <= self.h_cum_distance_list[zone]:
-                    return zone
-            return self.h_zone_number - 1
-    
-    def translate(self, v: float, zone: int, is_width: bool) -> float:
-        if zone == 0:
-            zone_boundary = 0
-        else:
-            zone_boundary = self.zone_boundary[zone-1]
+    # 將 src 座標轉換成 dst 座標
+    # boundary_list: [(50.0, 100.0), (100.0, 150.0), (150.0, 200.0)]
+    # distance_list: [(50.0, 100.0), (50.0, 50.0), (50.0, 50.0), (50.0, 100.0)]
+    def translate(self, src_pivot: int, n_zone: int, boundary_list: List[DstSrc], distance_list: List[DstSrc]) -> int:
+        zone = 0
+        not_found = True
+        for zone in range(n_zone):
+            if src_pivot <= boundary_list[zone].src:
+                not_found = False
+                break
+        if not_found:
+            zone = n_zone
 
-        # 將座標轉換為所屬區域的 0 ~ 1
-        v = (v-zone_boundary)/self.zone_distance[zone]
-        return v
+        pivot = src_pivot
+        # 若 zone 不為 0, 將座標根據所屬區塊的初始座標值做校正
+        if zone != 0:
+            pivot -= boundary_list[zone - 1].src
+        # 根據座標根據所屬區塊的長度, 將長度做校正
+        pivot = float(pivot) / distance_list[zone].src
+        pivot = pivot * distance_list[zone].dst
+        # 若 zone 不為 0, 將座標根據所屬區塊的初始座標值做校正
+        if zone != 0:
+            pivot += boundary_list[zone - 1].dst
+        return pivot
 
     # 輸出全壓縮圖像，即沒有注視哪一處，全局均勻壓縮
     def basic(self, img: cv2.typing.MatLike):
@@ -134,62 +133,27 @@ class OpenGaze:
         self.channel = shape[2]
         basic_img = cv2.resize(img, (self.WIDTH, self.HEIGHT), interpolation=cv2.INTER_CUBIC)
         return basic_img
-    
-    def setRadius(self, radius: float):
-        self.radius = radius
-        self.zone_boundary = [radius]
-        self.zone_distance = [radius]
-    
-    def getDistance(self, cx: float = 0, cy: float = 0, x: float = 0, y: float = 0) -> float:
-        distance = math.sqrt((x - cx)**2 + (y - cy)**2)
-        return distance
-    
-    def getWeight(self, distance: float = 0) -> float:
-        return 1.0 / (self.distance + distance)
-    
+
     def gaze(self, x: float = 0, y: float = 0): 
         weights = np.zeros((self.HEIGHT, self.WIDTH, 1), dtype=np.float32)
         values = np.zeros((self.HEIGHT, self.WIDTH, self.channel), dtype=np.float32)
-        w_i = x*self.width
-        h_i = y*self.height
+
+        w_zone_number, w_boundary_list, w_distance_list = self.initZone(x, self.WIDTH, self.width, self.zone_rate)
+        h_zone_number, h_boundary_list, h_distance_list = self.initZone(y, self.HEIGHT, self.height, self.zone_rate)
+
         X = None
         Y = None
         count = 0
 
         for h in range(self.height):
-            # 將 h 從原始像素座標，轉換為 0 ~ 1 之間的數值
-            hf = float(h) / float(self.height)
-            # dst_h = math.floor(hf*self.HEIGHT)
-
+            dst_h = self.translate(h, h_zone_number, h_boundary_list, h_distance_list)
             for w in range(self.width):
-                # 將 w 從原始像素座標，轉換為 0 ~ 1 之間的數值
-                wf = float(w) / float(self.width)
-                # dst_w = math.floor(wf*self.WIDTH)
+                dst_w = self.translate(w, w_zone_number, w_boundary_list, w_distance_list)
+
                 color = self.img[h, w]
-
-                # 計算和注視點的距離
-                w_distance = w_i - w
-                h_distance = h_i - h
-
-                # 計算 z 分數, 以及縮放比例
-                w_zone = self.getZone(distance=w_distance)
-                h_zone = self.getZone(distance=h_distance)
-                w_translate = self.translate(w, w_zone)
-                h_translate = self.translate(w, h_zone)
-                
-                dst_w = math.floor((w_zone + w_translate) * self.radius)
-                dst_h = math.floor((h_zone + h_translate) * self.radius)
-
-                if w_zone == 0 and h_zone == 0:
-                    values[dst_h, dst_w] = color
-                    # values[dst_h, dst_w] = np.zeros((1, 1, self.channel), dtype=np.float32)
-                    # values[dst_h, dst_w] = np.array([[[0, 0, 255]]])
-                    weights[dst_h, dst_w] = 1.0
-                else:
-                    distance = self.getDistance(w_i, h_i, h, w)
-                    weight = self.getWeight(distance)
-                    values[dst_h, dst_w] += color * weight
-                    weights[dst_h, dst_w] += weight  
+                values[dst_h, dst_w] += color
+                weights[dst_h, dst_w] += 1
+                     
 
         values /= weights        
         dst = np.zeros((self.HEIGHT, self.WIDTH, self.channel), dtype=np.uint8)
